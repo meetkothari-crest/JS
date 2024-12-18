e1b03f10bd4787f737f7e80b8da2ec9e2325766f
meetkothari-crest <meet.k@crestskillserve.com>
2024-12-18 11:24:02 +0530

    let, var, const keyword & datatypes

---
+++ basic/1_variables.js
@@ -0,0 +1,33 @@
+const name = "Meet"
+let surname = "Kothari"
+var age = 20
+
+address = "Surat, Gujarat"
+// name = "meet1"
+// console.log(name);
+
+// console.table([name, surname, age, address])
+
+
+let a = 10
+
+function callA()
+{
+
+    if(true)
+    {
+        let c = 1000
+    }
+    let b = 100
+    a = 5
+    console.log(a,b)
+}
+callA()
+console.log(a);
+
+
+
+/*
+var - functional scope or global scope
+let,const - block scope
+*/
---
+++ basic/2_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+/*
+
+number - 2 to power 53
+bigint - big numbers than number
+
+string - set of characters
+boolean - True/false
+
+null - standalone value
+undefined - value is not assigned 
+
+symbol - to find uniqueness of components
+
+object - 
+
+*/
+
+console.log(typeof "Meet");
+console.log(typeof null); // Object
+console.log(typeof undefined); // undefined
+console.log(typeof NaN); // number
+
